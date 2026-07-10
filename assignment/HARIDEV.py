93d1a85f0394579766c5c2ca3925ef25c7dd029b
sbharidev7-design <sbharidev7@gmail.com>
2026-07-10 13:10:45 +0530

    Add files via upload

---
+++ assignment/HARIDEV.py
@@ -0,0 +1,36 @@
+#3.comparison
+# a=10
+#b=10
+#print(a>b
+
+#logical
+#and,or,not
+#and
+
+#T T-->True
+#T f-->False
+#f f-->False
+# or
+
+# t f-->T
+# f f-->f
+# T T-->t
+#a=10
+#b=5
+#c=3
+
+#print(not(a>b or b<c))
+
+#4.membership
+#in ,not in
+#a='string'
+#print('i in a)
+#print('z' not in a)
+
+#identity
+#is is not
+#a='string'
+#b='string'
+#print(a==b)
+#print(a is b)
+#print(id(a))
